c91ceea569e557931d484cbfecd90b5bd420f805
Leon Si <leon@leondreamed.com>
2024-01-09 09:48:55 -0500

    feat: support property overrides

--- src/types/namespace.ts
+++ src/types/namespace.ts
@@ -1,20 +1,42 @@
-import { UnionToIntersection } from "type-fest";
+import type { UnionToIntersection } from "type-fest";
+
+type OverridePropertyKeys<
+  $Properties,
+  $OverrideSuffix extends string,
+> = keyof $Properties & `${string}${$OverrideSuffix}`;
+type PropertiesWithOverride<$Properties, $OverrideSuffix extends string> = Omit<
+  $Properties,
+  OverridePropertyKeys<
+    $Properties,
+    $OverrideSuffix
+  > extends `${infer $KeyToOverride}$`
+    ? $KeyToOverride
+    : never
+> & {
+  [K in OverridePropertyKeys<
+    $Properties,
+    $OverrideSuffix
+  > as K extends `${infer $KeyToOverride}${$OverrideSuffix}`
+    ? $KeyToOverride
+    : never]: K extends keyof $Properties ? $Properties[K] : never;
+};
 
 export type NestedNamespace<
-  Properties,
-  Separator extends string = "_",
+  $Properties,
+  $Separator extends string = "_",
+  $OverrideSuffix extends string = "$",
 > = UnionToIntersection<
   {
-    [K in keyof Properties]: {
-      [Key in K extends `${infer Namespace}${Separator}${string}`
-        ? Namespace
+    [K in keyof PropertiesWithOverride<$Properties, $OverrideSuffix>]: {
+      [Key in K extends `${infer $Namespace}${$Separator}${string}`
+        ? $Namespace
         : never]: {
-        [Key in K extends `${string}${Separator}${infer MethodName}`
-          ? MethodName
-          : never]: Properties[K];
+        [Key in K extends `${string}${$Separator}${infer $MethodName}`
+          ? $MethodName
+          : never]: PropertiesWithOverride<$Properties, $OverrideSuffix>[K];
       };
     };
-  }[keyof Properties]
+  }[keyof PropertiesWithOverride<$Properties, $OverrideSuffix>]
 >;
 
 export type FlatNamespace<
--- src/utils/namespace.ts
+++ src/utils/namespace.ts
@@ -2,17 +2,17 @@ import mapObject from "map-obj";
 import { FlatNamespace, NestedNamespace } from "~/types/namespace.js";
 
 export function createFlatNamespace<
-  Prefix extends string,
-  Properties extends Record<string, unknown>,
-  Separator extends string = "_",
+  $Prefix extends string,
+  $Properties extends Record<string, unknown>,
+  $Separator extends string = "_",
 >(
-  prefix: Prefix,
-  properties: Properties,
+  prefix: $Prefix,
+  properties: $Properties,
   options?: {
-    separator?: Separator;
+    separator?: $Separator;
     transformProperty?(args: { propertyKey: string; property: any }): any;
   }
-): FlatNamespace<Prefix, Properties, Separator> {
+): FlatNamespace<$Prefix, $Properties, $Separator> {
   const separator = options?.separator ?? "_";
   return mapObject(properties, (key, property) => {
     const propertyKey = (key as string).replace(`${prefix}${separator}`, "");
@@ -29,25 +29,48 @@ export function createFlatNamespace<
 }
 
 export function createNestedNamespace<
-  Properties extends Record<string, unknown>,
-  Separator extends string = "_",
+  $Properties extends Record<string, unknown>,
+  $Separator extends string = "_",
+  $OverrideSuffix extends string = "$",
 >(
-  properties: Properties,
+  properties: $Properties,
   options?: {
-    separator?: Separator;
+    separator?: $Separator;
+    overrideSuffix: $OverrideSuffix;
     transformProperty?(args: {
       namespace: string;
       propertyKey: string;
       property: any;
     }): any;
   }
-): NestedNamespace<Properties, Separator> {
+): NestedNamespace<$Properties, $Separator, $OverrideSuffix> {
   const namespaces: Record<string, Record<string, unknown>> = {};
   const separator = options?.separator ?? "_";
+  const overrideSuffix = options?.overrideSuffix ?? "$";
 
-  // We bind the `this` type of all library methods to the library
-  for (const [propertyIdentifier, property] of Object.entries(properties)) {
-    const [namespace, propertyKey] = propertyIdentifier.split(separator);
+  const propertyIdentifiers = Object.keys(properties).sort((p1, p2) => {
+    // Sort properties that end in the override suffix to the beginning
+    const p1EndsWithOverrideSuffix = p1.endsWith(overrideSuffix);
+    const p2EndsWithOverrideSuffix = p2.endsWith(overrideSuffix);
+
+    if (p1EndsWithOverrideSuffix && !p2EndsWithOverrideSuffix) {
+      return -1;
+    }
+    if (!p1EndsWithOverrideSuffix && p2EndsWithOverrideSuffix) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  for (const propertyIdentifier of propertyIdentifiers) {
+    // We bind the `this` type of all library methods to the library
+    let [namespace, propertyKey] = propertyIdentifier.split(separator);
+    const property = properties[propertyIdentifier];
+    if (propertyKey?.endsWith(overrideSuffix)) {
+      propertyKey = propertyKey.slice(0, -overrideSuffix.length);
+    }
+
     if (namespace === undefined || propertyKey === undefined) {
       console.warn(
         "Invalid property identifier, skipping:",
@@ -58,14 +81,16 @@ export function createNestedNamespace<
 
     namespaces[namespace] ??= {};
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- Guaranteed to exist
-    namespaces[namespace]![propertyKey] =
-      options?.transformProperty === undefined
-        ? property
-        : options.transformProperty({
-            namespace,
-            propertyKey,
-            property,
-          });
+    if (namespaces[namespace]![propertyKey] !== undefined) {
+      namespaces[namespace]![propertyKey] =
+        options?.transformProperty === undefined
+          ? property
+          : options.transformProperty({
+              namespace,
+              propertyKey,
+              property,
+            });
+    }
   }
 
   return namespaces as any;
