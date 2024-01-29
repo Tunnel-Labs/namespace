fdd06ed72502b06d0b4c43bfd90767b31c4efb3f
Leon Si <leon@leondreamed.com>
2024-01-29 11:35:40 -0500

    chore: re-export types

---
+++ exports/main.ts
@@ -0,0 +1,10 @@
+export type {
+	FlatNamespace,
+	NestedNamespace,
+	OverridePropertyKeys,
+	PropertiesWithOverride,
+} from '../types/namespace.ts';
+export {
+	createFlatNamespace,
+	createNestedNamespace,
+} from '../utils/namespace.ts';
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { createFlatNamespace, createNestedNamespace } from './utils/namespace.js';
-export type { FlatNamespace, NestedNamespace } from './types/namespace.js'
